387f2ad968d94a3b6b921c8e0a9d8586769b60c5
Cefalu Wave <luke.evan.graham@gmail.com>
2020-08-21 01:02:58 -0700

    nav buttons working

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // Global app controller
 import Search from "./models/Search";
 import * as searchView from "./views/searchView";
-import { elements } from "./views/base";
+import { elements, renderLoader, clearLoader } from "./views/base";
 
 // Global state of the app
 /********
@@ -25,11 +25,13 @@ const controlSearch = async () => {
   // 3) Prepare UI for results
   searchView.clearInput();
   searchView.clearResults();
+  renderLoader(elements.searchRes);
 
   // 4) Search for recipes
   await state.search.getResults();
 
   // 5) Render results on UI
+  clearLoader()
   searchView.renderResults(state.search.result)
 };
 
@@ -37,3 +39,12 @@ elements.searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   controlSearch();
 });
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline')
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage)
+    }
+})
