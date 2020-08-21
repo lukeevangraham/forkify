a87ca39639cde5b83342a779011b05099f6280b5
Cefalu Wave <luke.evan.graham@gmail.com>
2020-08-20 21:12:09 -0700

    webpack and babel set up

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 // Global app controller
 import num from './test'
+const x = 23;
 
-console.log(`I imported ${num} from another module!`);
+console.log(`I imported ${num} from test.js! Variable x is ${x}`);
