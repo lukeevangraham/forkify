18f4779a957017f9a406136b1ef3b5dfbfaf51bf
Cefalu Wave <luke.evan.graham@gmail.com>
2020-08-20 23:15:52 -0700

    titles render

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -6,6 +6,24 @@ export const clearInput = () => elements.searchInput.value = '';
 
 export const clearResults = () => elements.searchResList.innerHTML = '';
 
+// Pasta with tomato and spinach
+
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = []
+    if (title.length > limit) {
+        title.split(" ").reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+}
+
+
 const renderRecipe = (recipe) => {
   const markup = `
                 <li>
@@ -14,7 +32,7 @@ const renderRecipe = (recipe) => {
                             <img src="${recipe.image_url}" alt="${recipe.title}">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
+                            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
