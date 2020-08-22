32e80ac9e22d3a8a1c6555fdf1601439171c8c5e
Cefalu Wave <luke.evan.graham@gmail.com>
2020-08-21 23:36:19 -0700

    recipie renders except for serving changes

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
 import * as searchView from "./views/searchView";
+import * as recipeView from "./views/recipeView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 
 // Global state of the app
@@ -21,6 +22,7 @@ const state = {};
 const controlSearch = async () => {
   // Get query from view
   const query = searchView.getInput() /// TODO
+
   console.log(query)
 
   if (query) {
@@ -78,24 +80,31 @@ const controlRecipe = async () => {
 
   if (id) {
     // Prepare the UI for changes
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
+
+    // Highlight selected search item
+    if (state.search) {
+      searchView.highlightSelected(id)
+    }
 
     // Create new recipe object
     state.recipe = new Recipe(id);
 
     try {
-
-      
-      // Get recipe data
+      // Get recipe data and parse ingredients
       await state.recipe.getRecipe();
+      state.recipe.parseIngredients()
       
       // Calc servings and time
       state.recipe.calcTime();
       state.recipe.calcServings();
       
       // Render recipe
-      console.log(state.recipe);
+      clearLoader();
+      recipeView.renderRecipe(state.recipe)
     } catch (err) {
-      alert('Error processing recipe')
+      console.log('Error processing recipe', err)
     }
       
     }
