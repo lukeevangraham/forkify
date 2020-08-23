f2c42da9b914ffa7da32c5c4314b5e50a73e56e4
Cefalu Wave <luke.evan.graham@gmail.com>
2020-08-22 23:53:33 -0700

    list view prepared

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Global app controller
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
+import List from "./models/List";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
 import { elements, renderLoader, clearLoader } from "./views/base";
@@ -132,3 +133,5 @@ elements.recipe.addEventListener('click', e => {
   }
   console.log(state.recipe)
 })
+
+window.l = new List();
