3de7a86fc77d8036c75405ef1a0a0bb510904812
Cefalu Wave <luke.evan.graham@gmail.com>
2020-08-23 09:42:05 -0700

    getting ready to work on the likes controller

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import Recipe from "./models/Recipe";
 import List from "./models/List";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
+import * as listView from "./views/listView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 
 // Global state of the app
@@ -14,6 +15,7 @@ import { elements, renderLoader, clearLoader } from "./views/base";
  *  - Liked recipes
  */
 const state = {};
+window.state = state;
 
 /******
  * 
@@ -117,6 +119,39 @@ const controlRecipe = async () => {
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe))
 
+/***
+ * LIST CONTROLLER
+ */
+const controlList = () => {
+  // Create a new list if there is non yet
+  if (!state.list) state.list = new List();
+
+  // Add each ingredient to the list and UI
+  state.recipe.ingredients.forEach(el => {
+    const item = state.list.addItem(el.count, el.unit, el.ingredient)
+    listView.renderItem(item)
+  })
+}
+
+// Handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+  const id = e.target.closest('.shopping__item').dataset.itemid;
+
+  // Handle the delete
+  if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+    // Delete from state
+    state.list.deleteItem(id);
+
+    // Delete from UI
+    listView.deleteItem(id);
+
+    // Handle the count update
+  } else if (e.target.matches('.shopping__count-value')) {
+    const val = parseFloat(e.target.value, 10);
+    state.list.updateCount(id, val)
+  }
+})
+
 
 // Handling receipe button clicks
 elements.recipe.addEventListener('click', e => {
@@ -130,8 +165,9 @@ elements.recipe.addEventListener('click', e => {
     // Decrease button is clicked
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe);
+  } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    controlList();
   }
-  console.log(state.recipe)
 })
 
 window.l = new List();
